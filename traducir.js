5fc2fc495f6d909f197e8aa1d127aa76c7b1567d
Luca Lazcano <lazcanoluca@gmail.com>
2022-04-21 15:03:51 -0300

    hdfh

--- traducir.js
+++ traducir.js
@@ -5,7 +5,7 @@ const reverso = new Reverso();
 module.exports = async function traducir( texto, de_leng = 'Spanish', a_leng = 'English' ) {
     reverso.getTranslation(texto, de_leng, a_leng, (response) => {
         // console.log(response.translation[0]);
-        return response.translation[0];
+        return response[0];
         // console.log(response);
     }).catch((err) => {
         console.error(err);
