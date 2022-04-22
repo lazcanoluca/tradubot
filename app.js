ec3183508b4a7f2b6cee0571a6603bec956dfe22
Luca Lazcano <lazcanoluca@gmail.com>
2022-04-21 23:42:33 -0300

    traductor

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const traducir = require('./traducir.js');
+const { traducir } = require('./traductor');
 
-var traduccion = traducir('dia');
-console.log(traduccion);
+function unaVezTraducido( {translation} ) {
+    console.log(translation[0]);
+    // acá va lo que tenga que imprimir el bot
+}
+
+// ejemplo uso funcion
+const texto = 'Il y a certainement des gens heureux de vivre, dont les jouissances ne ratent pas et qui se gorgent de bonheur et de succès.';
+const desde = 'French';
+const hasta = 'German';
+traducir( {texto: texto, orig_len: desde, target_len: hasta, callback: unaVezTraducido} );
---
+++ traductor.js
@@ -0,0 +1,7 @@
+const Reverso = require('reverso-api');
+const reverso = new Reverso();
+
+exports.traducir = async function ( {texto, orig_len = 'Spanish', target_len = 'English', callback = unaVezTraducido} = {} ) {
+    const result = await reverso.getTranslation(texto, orig_len, target_len).catch(err => console.error(err));
+    callback(result);
+}
